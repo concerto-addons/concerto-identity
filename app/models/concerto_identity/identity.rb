60fb223fcba2cc2beb15d7d604cb7eda15228ed4
August <august@inniogroup.com>
2014-10-28 18:00:57 -0400

    set_table_name was deprecated back in Rails 3.1 or so

--- app/models/concerto_identity/identity.rb
+++ app/models/concerto_identity/identity.rb
@@ -1,7 +1,6 @@
 module ConcertoIdentity
   class Identity < ActiveRecord::Base
-
-    set_table_name 'concerto_identities'
+    self.table_name = 'concerto_identities'
     belongs_to :user
 
   end
